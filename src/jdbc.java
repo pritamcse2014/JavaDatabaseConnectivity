7265f96247ec6600616c654d0c51dc0bfc5a5b33
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-15 22:18:22 +0600

    ConnectingJavaAndDB

---
+++ src/jdbc.java
@@ -0,0 +1,23 @@
+import java.sql.*;
+
+public class jdbc {
+    static void main(String[] args) throws Exception {
+        /*
+            1. import Package
+            2. load and register
+            3. create connection
+            4. create statement
+            5. execute statement
+            6. process the results
+            7. close
+         */
+
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String user = "postgres";
+        String password = "2014";
+
+        /* Class.forName("org.postgresql.Driver"); */
+        Connection connection = DriverManager.getConnection(url, user, password);
+        System.out.println("Database Connection Established....");
+    }
+}
