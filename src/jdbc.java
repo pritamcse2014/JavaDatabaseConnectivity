34282eb646ad39ad49a705b4f2c2b8a5a27c807b
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-16 11:38:13 +0600

    FetchingAllRecords

--- src/jdbc.java
+++ src/jdbc.java
@@ -15,7 +15,7 @@ public class jdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String user = "postgres";
         String password = "2014";
-        String sql = "Select sname from student where sid = 1";
+        String sql = "Select * from student";
 
         /* Class.forName("org.postgresql.Driver"); */
         Connection connection = DriverManager.getConnection(url, user, password);
@@ -24,9 +24,16 @@ public class jdbc {
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
         /* System.out.println(resultSet.next()); */
+        /*
         resultSet.next();
         String name = resultSet.getString("sname");
         System.out.println("Name of Student : " + name);
+        */
+        while (resultSet.next()) {
+            System.out.print(resultSet.getInt(1) + " - ");
+            System.out.print(resultSet.getString(2) + " - ");
+            System.out.println(resultSet.getInt(3));
+        }
         connection.close();
         System.out.println("Database Connection Closed....");
     }
