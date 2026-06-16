5a70cf2ea773e05d5fb14432be86262039af0291
Pritam Kumar Kundu <pritam.cse2014@gmail.com>
2026-06-16 11:22:01 +0600

    ExecuteAndProcess

--- src/jdbc.java
+++ src/jdbc.java
@@ -15,9 +15,19 @@ public class jdbc {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String user = "postgres";
         String password = "2014";
+        String sql = "Select sname from student where sid = 1";
 
         /* Class.forName("org.postgresql.Driver"); */
         Connection connection = DriverManager.getConnection(url, user, password);
         System.out.println("Database Connection Established....");
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(sql);
+        /* System.out.println(resultSet.next()); */
+        resultSet.next();
+        String name = resultSet.getString("sname");
+        System.out.println("Name of Student : " + name);
+        connection.close();
+        System.out.println("Database Connection Closed....");
     }
 }
